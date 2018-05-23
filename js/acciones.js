a20f26ebbef96f35dfb86ffe71c5839712a95538
josmaymaca <bot.flnetwork@gmail.com>
2018-05-23 11:15:09 -0500

    Subida3

--- js/acciones.js
+++ js/acciones.js
@@ -4,21 +4,21 @@ $(document).ready(function (e) {
 	document.addEventListener("Deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizó a la izquierda",function(){"Aplicacion7","Aceptar"});
+			navigator.notification.alert("Deslizó a la izquierda",function(){"Aplicacion7","OK"});
 		});
 		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Qué quieres hacer?",function(opt){
-				switch(opt)
+			navigator.notification.confirm("¿Qué deseas hacer?",function(op){
+				switch(op)
 				{
 					case 1:
 					navigator.notification.beep(1);
 					break;
 					
 					case 2:
-					navigator.notification.vibrate(1000);
+					navigator.notification.vibrate(1500);
 					break;
 				}
-			},"Aplicacion7","Beep,Vibrar,Cancelar");	
-	});
-},false);
+			},"Aplicacion7","Sonar,Vibrar,Cancelar");	
+		});
+	},false);
 });
